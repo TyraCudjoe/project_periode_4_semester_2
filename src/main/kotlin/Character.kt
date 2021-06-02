cf5dc6f9a415b0ab3113f7c58efd4104df849f49
tyra cudjoe <tyra20015@gmail.com>
2021-06-02 11:56:56 +0200

    added super class Character.kt and added text to Main.kt

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,3 +1,24 @@
-open class Character(val name: String, var life: Int = 3, var level: Int = 1, var score: Int = 0) {
+open class Character(val name: String, var life: Int = 3, var level: Int = 1, var score: Int = 0, var hitpoints: Int) {
+    open fun takeDamage(damage: Int) {
+        val remainingHitpoints = hitpoints - damage
+        if (remainingHitpoints > 0){
+            hitpoints = remainingHitpoints
+            println("$name took $damage points of damage and has  $hitpoints left.")
+        }else {
+            if (life > 0){
+                life -= 1
+                println("$name has lost a life.")
+            }else {
+                println("$name is dead.")
+            }
+        }
+    }
 
+    override fun toString(): String {
+        return  """
+           name:        $name
+           hitpoints:   $hitpoints
+           life:        $life
+                """
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,23 @@ fun main(args: Array<String>){
     val travon = Player("Travon", 3, 2, 3)
     val trol = Enemy("troll", 3, 6,0)
 
-    println("$travon")
+
+    println("Welcome to 'WENT BAD!'")
+    println("This will be the trippiest text based game you will ever play.")
+    println("And maybe one day, it will be a 2D or 3D game, but who knows")
+    println("When you're ready to start type 'ready'")
+    val enteredString = readLine()
+//    if (readLine() = "ready") {
+//
+//    }
+    println("")
+    println("")
+    println("")
+    println("")
+    println("")
+    println("")
+    println("")
+    println("")
 
 
 }
