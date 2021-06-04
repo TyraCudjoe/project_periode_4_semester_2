f573f00c206c608716d74ab8427398d5ee069255
tyra cudjoe <tyra20015@gmail.com>
2021-06-04 14:27:55 +0200

    commit 04-06-2021

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,33 @@
 fun main(args: Array<String>){
-    val travon = Player("Travon", 3, 2, 3)
-    val trol = Enemy("troll", 3, 6,0)
+    val travon = Player("Travon", 3, 2, 3, 4)
+    val trol = Enemy("troll", 3, 6,0, 2)
 
 
     println("Welcome to 'WENT BAD!'")
     println("This will be the trippiest text based game you will ever play.")
     println("And maybe one day, it will be a 2D or 3D game, but who knows")
     println("When you're ready to start type 'ready'")
-    val enteredString = readLine()
-//    if (readLine() = "ready") {
-//
-//    }
-    println("")
-    println("")
-    println("")
-    println("")
-    println("")
-    println("")
-    println("")
-    println("")
+
+    var enteredString = readLine()
+    var line = "ready"
+    while (enteredString!!.isBlank()) {
+        println("please enter 'ready'")
+        enteredString = readLine()
+//        else {
+//            line = "ready"
+//            println("you are going to the next level")
+//        }
+
+    }
+
+//    println("")
+//    println("")
+//    println("")
+//    println("")
+//    println("")
+//    println("")
+//    println("")
+//    println("")
 
 
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,4 +1,4 @@
-class Player(name: String, life: Int = 3, level: Int = 3, score: Int = 0) : Character(name, life, level, score) {
+class Player(name: String, life: Int = 3, level: Int = 3, score: Int = 0, Hitpoints: Int) : Character(name, life, level, score, Hitpoints) {
     var weapon: Weapon = Weapon("Fists", 6)
     val inventory = ArrayList<Loot>()
 
