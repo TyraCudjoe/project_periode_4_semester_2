f3678e4874e1818cd169388c843afc6f998c7497
DamienF <329877@student.mboutrecht.nl>
2021-06-09 10:38:10 +0200

    type: addition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,39 @@
-fun main(args: Array<String>){
-    val travon = Player("Travon", 3, 2, 3, 4)
-    val trol = Enemy("troll", 3, 6,0, 2)
+fun main(args: Array<String>) {
+//    val travon = Player("Travon", 3, 2, 3, 4)
+//    val trol = Enemy("troll", 3, 6, 0, 2)
+
+    println("Write anything here: ")
+    println()
+
+    val enteredString = readLine()
+    println("You have entered this: $enteredString")
+
+//    println("Welcome to 'WENT BAD!'")
+//    println("This will be the trippiest text based game you will ever play.")
+//    println("And maybe one day, it will be a 2D or 3D game, but who knows")
+//    println("Pleas enter your character name")
+//
+//    val username = readLine()
+
+//    while (username!!.isBlank()) {
+//        println(username!!.isBlank())
+//        println("Pleas enter a valid username")
+//        username = readLine()
+//    }
+//    println("Are you ready to trip $username?")
 
 
-    println("Welcome to 'WENT BAD!'")
-    println("This will be the trippiest text based game you will ever play.")
-    println("And maybe one day, it will be a 2D or 3D game, but who knows")
-    println("When you're ready to start type 'ready'")
-
-    var enteredString = readLine()
-    var line = "ready"
-    while (enteredString!!.isBlank()) {
-        println("please enter 'ready'")
-        enteredString = readLine()
-//        else {
+//    var enteredString = readLine()
+//    var line = "ready"
+//    while (enteredString!!.isBlank()) {
+//        println("please enter 'ready'")
+//        enteredString = readLine()
+////        else {
 //            line = "ready"
 //            println("you are going to the next level")
 //        }
 
-    }
+
 
 //    println("")
 //    println("")
